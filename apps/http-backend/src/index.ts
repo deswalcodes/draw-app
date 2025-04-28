a655efbf368163e6b72b787147a0cc5c9cf34b4f
deswalcodes <21cs2014@rgipt.ac.in>
2025-04-29 00:45:33 +0530

    added signup route

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,20 +3,40 @@ import jwt from 'jsonwebtoken'
 import { JWT_SECRET } from '@repo/backend-common/config';
 import { middleware } from './middleware';
 import {CreateUserSchema,SigninSchema,CreateRoomSchema} from "@repo/common/types"
+import {prismaClient} from "@repo/db/client"
 
 const app= express();
+app.use(express.json())
 
 
 
-app.post('/signup',(req,res)=>{
-   const data = CreateUserSchema.safeParse(req.body)
-   if(!data.success){
+app.post('/signup',async(req,res)=>{
+   const parsedData = CreateUserSchema.safeParse(req.body)
+   if(!parsedData.success){
     res.json({
         message : "Incorrect Inputs"
     })
     return
    }
+   try{
+    const user = await prismaClient.user.create({
+        data : {
+         email : parsedData.data.username,
+         password : parsedData.data.password,
+         name : parsedData.data.name
+        }
+        })
+        res.json({
+         message : "Profile Created!",
+         userId : user.id
+        })
+     
+   } catch(err){
+    res.status(411).json({
+        message : "User already exists!"
+    })
 
+   }
 
 })
 
